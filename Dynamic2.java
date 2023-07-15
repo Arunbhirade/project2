e23721807320d38e579ffd5c7909dcdecf74d065
Arunbhirade <arunbhirade0707@gmail.com>
2023-07-15 17:46:08 +0530

    dynamic code

---
+++ Dynamic2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Dynamic2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a;i>=b;i--)
+		{
+		System.out.println(i);
+
+		}
+	}
+}
