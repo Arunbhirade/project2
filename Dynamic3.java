01e97acdc8b663bee03a5e354b7042a872c0c382
Arunbhirade <arunbhirade0707@gmail.com>
2023-07-15 17:32:52 +0530

    dynamic3 java program

---
+++ Dynamic3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Dynamic3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Value");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		for(int i=a;i<=b;i++)
+		{
+		if(i%2==0)
+			{
+		System.out.println(i);
+		  }
+		}
+	}
+}
